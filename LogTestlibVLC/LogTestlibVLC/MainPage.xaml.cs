176c57b5b4c5ff80cf606d88890e94b87ca96df5
Michael McConnell <michael@winkstreaming.com>
2023-05-30 20:42:22 -0600

    Add project files.

---
+++ LogTestlibVLC/LogTestlibVLC/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using LibVLCSharp.Shared;
+using LibVLCSharp.Forms.Shared;
+
+namespace LogTestlibVLC
+{
+    public partial class MainPage : ContentPage
+    {
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            var media = new Media(_libVLC, "rtsp://viewer:prados123@192.168.1.82", FromType.FromLocation);
+            _mediaPlayer.Play(media);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            _mediaPlayer.Stop();
+        }
+    }
+}
